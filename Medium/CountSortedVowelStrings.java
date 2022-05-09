0576e8092faac2a7acdeccae0c53cfe6a2ccf645
Kritartha <68478501+Kritartha1@users.noreply.github.com>
2022-05-09 21:53:35 +0530

    Update CountSortedVowelStrings.java

--- Medium/CountSortedVowelStrings.java
+++ Medium/CountSortedVowelStrings.java
@@ -1,5 +1,6 @@
 //leetcode.com/problems/count-sorted-vowel-strings/
 
+//Approach 1: Top down
 class Solution {
     
     char[] v={'a','e','i','o','u'};
@@ -72,4 +73,49 @@ class Solution {
         
     }
     
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Approach 2:
+class Solution {
+    public int countVowelStrings(int n) {
+        
+        // time: O(n)
+        // space: O(1)
+        // just think about the mathematical induction
+        // if we know all the string of len k, so the string of len k + 1 will be
+        // 1 add a before all string of len k
+        // 2 add e before the string of len k, which is starts with or after e
+        // 3 add i before the string of len k, which starts with or after i
+        // 4 add o before the string of len k, which starts with or after o
+        // 5 add u before the string of len k, which starts with or after u
+        
+        int a=1,e=1,i=1,o=1,u=1;
+        
+        while(n>1){
+            a=a+e+i+o+u;  // a, e, i, o, u -> aa, ae, ai, ao, au
+            e=e+i+o+u;    // e, i, o, u -> ee, ei, eo, eu 
+            i=i+o+u;      // i, o, u -> ii, io, iu
+            o=o+u;        // o, u -> oo, ou
+            u=u;          // u -> uu
+            --n;
+        }
+        
+        return a+e+i+o+u;
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Appraoch 3: P&C
+class Solution {
+    public int countVowelStrings(int n) {
+        
+        //(n+4)C4 or (n+4)Cn
+        
+        //https://leetcode.com/problems/count-sorted-vowel-strings/discuss/918498/JavaC++Python-DP-O(1)-Time-and-Space/822478
+        
+        //Simple Permutation and combination using stick method...or partition method
+        return ((n+4)*(n+3)*(n+2)*(n+1))/24;
+    }
 }
